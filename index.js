3bfa0304d41d5af64a7ecf8509b0fd3de39174cc
unknown <cristian_manuel@outlook.com>
2023-09-28 20:10:00 -0500

    u

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(
 )
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.static("public"));
+//app.use(express.static("public"));
 app.use('/', redirectRouter) //-optional
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/links', linkRouter) //
